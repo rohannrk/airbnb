7a1b12783cd3b481cf8de1252df51f3229043f6d
Rohan <rohanrk39357@gmail.com>
2023-08-07 16:40:47 +0530

    List creation Step 1

--- app/components/modals/LoginModal.tsx
+++ app/components/modals/LoginModal.tsx
@@ -59,6 +59,11 @@ const LoginModal = () => {
         })
     }
 
+    const toggle = useCallback(() => {
+        LoginModal.onClose();
+        registerModal.onOpen();
+    }, [LoginModal, registerModal]);
+
     const bodyContent = (
         <div className='flex flex-col gap-4'>
             <Heading
@@ -110,16 +115,16 @@ const LoginModal = () => {
             >
                 <div className='justify-center flex flex-row items-center gap-2'>
                     <div>
-                        Already have an account?
+                        First time using Airbnb?
                     </div>
                     <div
-                    onClick={registerModal.onClose}
+                    onClick={toggle}
                     className='
                     text-neutral-800
                     cursor-pointer
                     hover:underline
                     '>
-                        Log in
+                        Create an account
                     </div>
                 </div>
 
--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -8,6 +8,7 @@ import useRegisterModal from '@/app/hooks/useRegisterModal';
 import useLoginModal from '@/app/hooks/useLoginModal';
 import { signOut } from 'next-auth/react';
 import { SafeUser } from '@/app/types';
+import useRentModal from '@/app/hooks/useRentModal';
 
 
 interface UserMenuProps {
@@ -18,18 +19,28 @@ const UserMenu: React.FC<UserMenuProps> = ({
     currentUser
 }) => {
     const registerModal = useRegisterModal();
-    const LoginModal = useLoginModal();
+    const loginModal = useLoginModal();
+    const rentModal = useRentModal();
     const [isOpen, setIsOpen] = useState(false);
 
     const toggleOpen = useCallback(() => {
         setIsOpen((value) => !value);
     }, []);
 
+    const onRent = useCallback(() => {
+        if(!currentUser) {
+            return loginModal.onOpen();
+        }
+
+        rentModal.onOpen();
+
+    }, [currentUser, loginModal, rentModal]);
+
     return (
         <div className="relative">
             <div className="flex flex-row items-center gap-3">
                 <div
-                    onClick={() => { }}
+                    onClick={onRent}
                     className="
                 hidden
                 md:block
@@ -105,7 +116,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                                     label="My Properties"
                                 />
                                 <MenuItem
-                                    onClick={()=> {}}
+                                    onClick={rentModal.onOpen}
                                     label="Airbnb my home"
                                 />
                                 <hr/>
@@ -117,7 +128,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                         ) : (
                         <>
                             <MenuItem
-                                onClick={LoginModal.onOpen}
+                                onClick={loginModal.onOpen}
                                 label="Login"
                             />
                             <MenuItem
