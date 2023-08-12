08f56cf9285500d1a1ee23ac5a61ddcce271f6d2
Rohan <rohanrk39357@gmail.com>
2023-08-12 23:46:40 +0530

    Trips screen and fetching reservations

--- app/components/navbar/UserMenu.tsx
+++ app/components/navbar/UserMenu.tsx
@@ -9,6 +9,7 @@ import useLoginModal from '@/app/hooks/useLoginModal';
 import { signOut } from 'next-auth/react';
 import { SafeUser } from '@/app/types';
 import useRentModal from '@/app/hooks/useRentModal';
+import { useRouter } from 'next/navigation';
 
 
 interface UserMenuProps {
@@ -18,6 +19,7 @@ interface UserMenuProps {
 const UserMenu: React.FC<UserMenuProps> = ({
     currentUser
 }) => {
+    const router = useRouter();
     const registerModal = useRegisterModal();
     const loginModal = useLoginModal();
     const rentModal = useRentModal();
@@ -100,7 +102,7 @@ const UserMenu: React.FC<UserMenuProps> = ({
                         {currentUser ? (
                             <>
                                 <MenuItem
-                                    onClick={()=> {}}
+                                    onClick={()=> router.push("/trips")}
                                     label="My trips"
                                 />
                                 <MenuItem
