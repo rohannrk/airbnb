b12646f9c687048d5195832167bce385719827df
Rohan <rohanrk39357@gmail.com>
2023-08-13 00:54:56 +0530

    Search and Sort Functionality & Errror and Loading State

--- app/layout.tsx
+++ app/layout.tsx
@@ -8,6 +8,7 @@ import LoginModal from './components/modals/LoginModal';
 import ToasterProvider from './providers/ToasterProvider';
 import getCurrentUser from './actions/getCurrentUser';
 import RentModal from './components/modals/RentModal';
+import SearchModal from './components/modals/SearchModal';
 
 const font = Nunito({ subsets: ['latin'] });
 
@@ -30,6 +31,7 @@ export default async function RootLayout({
         <ClientOnly>
           <ToasterProvider/>
           <RentModal/>
+          <SearchModal/>
           <RegisterModal/>
           <LoginModal/>
           <Navbar currentUser={currentUser}/>
